c6f12220c0d24649b00dcad7bdfc26f6f18b5b87
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-27 21:47:19 +0430

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // import NavbarComponent from './components/NavbarComponent';
-import Hero from './components/Hero';
-import AboutUs from './components/AboutUs';
-import Services from './components/Services';
+import Hero from './Component/Hero';
+import AboutUs from './Component/AboutUs';
+import Services from './Component/Services';
 
 
 function App() {
