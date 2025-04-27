e1e7f7394691e1ec067e46d240dda18948c88e6a
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-27 21:50:46 +0430

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import NavbarComponent from './components/NavbarComponent';
+import NavbarComponent from './Component/NavbarComponent';
 import Hero from './Component/Hero';
 import AboutUs from './Component/AboutUs';
 import Services from './Component/Services';
@@ -8,7 +8,7 @@ import Services from './Component/Services';
 function App() {
   return (
     <div className="App">
-      {/* <NavbarComponent /> */}
+      <NavbarComponent />
       <Hero />
       <AboutUs />
       <Services />
